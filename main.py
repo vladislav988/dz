e4142a44cada2a5e318ba9b40c1d009f593c69de
Neto test <zhuravlv-vlad@bk.ru>
2023-12-08 23:54:59 +0400

    commit

--- main.py
+++ main.py
@@ -1,39 +1,59 @@
-cook_book = {
-  'Омлет': [
-    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-    ],
-  'Утка по-пекински': [
-    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-    ],
-  'Запеченный картофель': [
-    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
-    ]
-  }
-
-def get_shop_list_by_dishes(dishes, person_count):
-    params = {}
-    for i in cook_book:
-        if i == dishes:
-            ingrs = {}
-            for j in cook_book[i]:
-                par = j
-                par['quantity'] = par['quantity'] * person_count
-                ingr = {par['ingredient_name']: {'measure': par['measure'], 'quantity': par['quantity']}}
-                ingrs.update(ingr)
-    print(ingrs)
-    # print(ingr)
+cook_book = {}
+ingredients_list = []
+from pprint import pprint
+with open("res.txt", encoding="UTF-8") as f:
+    a = 1
+    for i in f:
+        # print(a)
+        if a == 1:
+            dish_name = i.strip()
+        if a == 2:
+            ing_qty = int(i.strip())
+            print(ing_qty)
+            # print(ing_qty)
+            b = 0
+            while b != ing_qty:
+                dc_1 = []
+                print("hello")
+                ing_qty -= 1
+                ingredient_dict = {}
+                item1, item2, item3 = f.readline().split("|")
+                ingredient_dict["ingredient_name"] = item1.strip(' ')
+                ingredient_dict['quantity'] = item2.strip('')
+                ingredient_dict['measure'] = item3.strip(' \n')
+                a = -1
+                print(ingredient_dict)
+                dc_1.append(ingredient_dict)
+            ingredients_list.append(dc_1)
+            cook_book[dish_name] = ingredients_list
+            #подскажите пожалуйста почему смешываются ингредиенты
 
 
 
-get_shop_list_by_dishes("Омлет", 3)
+        a +=1
 
+
+ingredients_list.append(ingredient_dict)
+cook_book[dish_name] = ingredients_list
+pprint(cook_book)
+# print(dish_name)
+# def get_shop_list_by_dishes(dishes, person_count):
+#     params = {}
+#     for i in cook_book:
+#         if i == dishes:
+#             ingrs = {}
+#             print(cook_book)
+#             for j in cook_book[i]:
+#                 par = j
+#                 par['quantity'] = int(par['quantity']) * person_count
+#                 ingr = {par['ingredient_name']: {'measure': par['measure'], 'quantity': par['quantity']}}
+#                 ingrs.update(ingr)
+#     print(ingrs)
+#     # print(ingr)
+#
+#
+#
+# get_shop_list_by_dishes("Омлет", 3)
 
 
 
